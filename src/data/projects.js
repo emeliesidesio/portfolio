2e22008e0600c3a595f769e61619b1e2adee01c9
Emelie <esidesio@gmail.com>
2018-02-21 11:18:24 +0100

    updated projects on startpage

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,23 +1,23 @@
 export default [
   {
-    image: "https://s10.postimg.org/40bip0ph5/advent-cal-img.png",
-    id: "calendar",
-    name: "Christmas Calendar",
-    language: "JavaScript, HTML5, CSS",
-    link: "https://github.com/emeliesidesio/advent-calendar",
-    demo: "https://advent-calendar.netlify.com/code/",
-    description: "In this group assignment we were asked to create a traditional christmas advent calendar. The calendar should contain 24 days behind which a cookie recipe would be revelead. Each recipe page should have it's own CSS styling. It was also important that the visitor was not able to skip ahead and peek on a day in the future. Only the current day and days in the past should be possible to open. The rest of them should be locked. Since we didn't have a lot of time for this assignment we decided in the group to divide most of the work between us. I came up with the design, structured the landing page and made sure that it was responsive. We divided the styling for the 24 cookie recipes equally between us."
-  }, {
-    id: "forms",
-    image: "https://s13.postimg.org/dldyoex2v/forms-img-3.png",
-    name: "Signup Form",
-    language: "HTML5, CSS",
-    link: "https://github.com/emeliesidesio/signup-form",
-    demo: "https://emeliesidesio.github.io/assignment-forms/",
-    description: "In this assignment we had to build a signup form for any web service that should contain; text fields, password fields, radio buttons, checkboxes and a submit button. We also had to connect and post the data from the form to a server to be able to show the data that's put in."
+   id: "designbrief",
+   image: "https://s13.postimg.org/ozu86ivif/design-brief-screen.png",
+   name: "Design Brief",
+   language: "React, HTML5, CSS",
+   link: "https://github.com/emeliesidesio/design-brief",
+   demo: "https://emeliesidesio.github.io/design-brief/",
+   description: "The purpose of this assignment was to follow a design brief and copy the design as close as possible."
+ }, {
+    id: "finalproject",
+    image: "https://s13.postimg.org/tzroec3o7/final-project-screen.png",
+    name: "Final Project",
+    language: "React, JavaScript, HTML5, CSS, Node",
+    link: "https://github.com/emeliesidesio/ema",
+    demo: "http://seizetheparty.events/",
+    description: "For the final project the purpose was to use everything that we've learned this far. Me and my group wanted to create a service where you can create events and invite your friends. We build our own backend with MongoDB and Mongoose as well as a frontend. We implemented the service Sendgrid to be able to actually send invites to guests."
   }, {
     id: "habit",
-    image: "https://s14.postimg.org/c43b0jx35/habit-tracker-img.png",
+    image: "https://s13.postimg.org/sn5dyqbpz/habit-tracker-screens.png",
     name: "Habit Tracker Application",
     language: "React, JavaScript, HTML5, CSS",
     link: "https://github.com/emeliesidesio/habit-tracker-app",
@@ -25,23 +25,23 @@ export default [
     description: "This was a group project where we were supposed to create a habit tracker. An app where the user can store goals and keep track of their acheivements."
   }, {
     id: "clock",
-    image: "https://s13.postimg.org/ce5zw5qlz/clock-img-5.png",
+    image: "https://s13.postimg.org/qmjfrq4zb/clock-screen.png",
     name: "Build a Clock",
     language: "HTML5, CSS, JavaScript",
     link: "https://github.com/emeliesidesio/clock",
     demo: "https://emeliesidesio.github.io/clock/",
     description: "In this assignment we were supposed to create a clock using React state. A stretch goal included making a button which pauses the clock, and another one that starts it again."
   }, {
-    id: "tictail",
-    image: "https://s14.postimg.org/7chc0tonl/tictail-img.png",
-    name: "Create an Online Store",
-    language: "React, JavaScript, HTML5, CSS",
-    link: "https://github.com/emeliesidesio/online-store",
-    demo: "https://online-shop.netlify.com/",
-    description: "In this group assignment we had to create an online shop. The shop should contain one startpage listing all the products, a category page and a product detail page. We got help from Tictail and built the store using their API's. The site should also be responsive. In our group we decided early on that we wanted to work on all parts together and do a lot of pair programming. We wanted everyone to be involved and understand the different functions and components. We only divided work that we already knew how to do, like CSS styling and structure, and then we worked together on the parts that were new to us."
+      image: "https://s13.postimg.org/eje44btyv/advent-cal-screen.png",
+      id: "calendar",
+      name: "Christmas Calendar",
+      language: "JavaScript, HTML5, CSS",
+      link: "https://github.com/emeliesidesio/advent-calendar",
+      demo: "https://advent-calendar.netlify.com/code/",
+      description: "In this group assignment we were asked to create a traditional christmas advent calendar. The calendar should contain 24 days behind which a cookie recipe would be revelead. Each recipe page should have it's own CSS styling. It was also important that the visitor was not able to skip ahead and peek on a day in the future. Only the current day and days in the past should be possible to open. The rest of them should be locked. Since we didn't have a lot of time for this assignment we decided in the group to divide most of the work between us. I came up with the design, structured the landing page and made sure that it was responsive. We divided the styling for the 24 cookie recipes equally between us."
   }, {
     id: "grid",
-    image: "https://s14.postimg.org/ps1sy17v5/css-grid.png",
+    image: "https://s13.postimg.org/pbboix1k7/css-grids-screen.png",
     name: "CSS Grids",
     language: "React, HTML5, CSS",
     link: "https://github.com/emeliesidesio/css-grid",
