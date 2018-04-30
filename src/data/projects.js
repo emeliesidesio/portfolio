7b67ee4fa365ea8f35442f1064eb0b516d5e96d4
Emelie Sidesiö <esidesio@gmail.com>
2018-04-30 14:37:09 +0200

    updated image links

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,15 +1,15 @@
 export default [
   {
-   id: "designbrief",
-   image: "https://s13.postimg.org/ozu86ivif/design-brief-screen.png",
-   name: "Design Brief",
-   language: "React, HTML5, CSS",
-   link: "https://github.com/emeliesidesio/design-brief",
-   demo: "https://emeliesidesio.github.io/design-brief/",
-   description: "The purpose of this assignment was to follow a design brief and copy the design as close as possible."
- }, {
+    id: "designbrief",
+    image: "https://s14.postimg.cc/yfrvqn5u9/design-brief.png",
+    name: "Design Brief",
+    language: "React, HTML5, CSS",
+    link: "https://github.com/emeliesidesio/design-brief",
+    demo: "https://emeliesidesio.github.io/design-brief/",
+    description: "The purpose of this assignment was to follow a design brief and copy the design as close as possible."
+  }, {
     id: "finalproject",
-    image: "https://s13.postimg.org/tzroec3o7/final-project-screen.png",
+    image: "https://s14.postimg.cc/hf8zi2nox/seize-the-party.png",
     name: "Final Project",
     language: "React, JavaScript, HTML5, CSS, Node",
     link: "https://github.com/emeliesidesio/ema",
@@ -17,7 +17,7 @@ export default [
     description: "For the final project the purpose was to use everything that we've learned this far. Me and my group wanted to create a service where you can create events and invite your friends. We build our own backend with MongoDB and Mongoose as well as a frontend. We implemented the service Sendgrid to be able to actually send invites to guests."
   }, {
     id: "habit",
-    image: "https://s13.postimg.org/sn5dyqbpz/habit-tracker-screens.png",
+    image: "https://s14.postimg.cc/oz22jn3vl/habit-tracker.png",
     name: "Habit Tracker Application",
     language: "React, JavaScript, HTML5, CSS",
     link: "https://github.com/emeliesidesio/habit-tracker-app",
@@ -25,23 +25,23 @@ export default [
     description: "This was a group project where we were supposed to create a habit tracker. An app where the user can store goals and keep track of their acheivements."
   }, {
     id: "weather",
-    image: "https://s13.postimg.org/qjm8it16v/weather-dashboard-screen.png",
+    image: "https://s14.postimg.cc/ytt7pxsf5/weather-app.png",
     name: "Weather Dashboard",
     language: "HTML5, CSS, JavaScript",
     link: "https://github.com/emeliesidesio/weather-dashboard",
     demo: "https://emeliesidesio.github.io/weather-dashboard/index",
     description: "A project to build a simple weather dashboard with a weather API. It includes a function that changes the color of the backgound, depending on the temperature."
   }, {
-      image: "https://s13.postimg.org/eje44btyv/advent-cal-screen.png",
-      id: "calendar",
-      name: "Christmas Calendar",
-      language: "JavaScript, HTML5, CSS",
-      link: "https://github.com/emeliesidesio/advent-calendar",
-      demo: "https://advent-calendar.netlify.com/code/",
-      description: "In this group assignment we were asked to create a traditional christmas advent calendar. The calendar should contain 24 days behind which a cookie recipe would be revelead. Each recipe page should have it's own CSS styling. It was also important that the visitor was not able to skip ahead and peek on a day in the future. Only the current day and days in the past should be possible to open. The rest of them should be locked. Since we didn't have a lot of time for this assignment we decided in the group to divide most of the work between us. I came up with the design, structured the landing page and made sure that it was responsive. We divided the styling for the 24 cookie recipes equally between us."
+    image: "https://s14.postimg.cc/3mukm6p4x/calendar.png",
+    id: "calendar",
+    name: "Christmas Calendar",
+    language: "JavaScript, HTML5, CSS",
+    link: "https://github.com/emeliesidesio/advent-calendar",
+    demo: "https://advent-calendar.netlify.com/code/",
+    description: "In this group assignment we were asked to create a traditional christmas advent calendar. The calendar should contain 24 days behind which a cookie recipe would be revelead. Each recipe page should have it's own CSS styling. It was also important that the visitor was not able to skip ahead and peek on a day in the future. Only the current day and days in the past should be possible to open. The rest of them should be locked. Since we didn't have a lot of time for this assignment we decided in the group to divide most of the work between us. I came up with the design, structured the landing page and made sure that it was responsive. We divided the styling for the 24 cookie recipes equally between us."
   }, {
     id: "grid",
-    image: "https://s13.postimg.org/pbboix1k7/css-grids-screen.png",
+    image: "https://s14.postimg.cc/n4p829btt/css-grid.png",
     name: "CSS Grids",
     language: "React, HTML5, CSS",
     link: "https://github.com/emeliesidesio/css-grid",
@@ -49,7 +49,7 @@ export default [
     description: "An assignment with CSS grids. The task was to copy an already skethed design and come as close to the original as possible."
   }, {
     id: "clock",
-    image: "https://s13.postimg.org/qmjfrq4zb/clock-screen.png",
+    image: "https://s14.postimg.cc/5rexnl3pt/clock.png",
     name: "Build a Clock",
     language: "HTML5, CSS, JavaScript",
     link: "https://github.com/emeliesidesio/clock",
@@ -57,7 +57,7 @@ export default [
     description: "In this assignment we were supposed to create a clock using React state. A stretch goal included making a button which pauses the clock, and another one that starts it again."
   }, {
     id: "radio",
-    image: "https://s13.postimg.org/m974412d3/radio-stations-screen.png",
+    image: "https://s14.postimg.cc/u971axo4h/radio.png",
     name: "Radio Player",
     language: "React, JavaScript, HTML5, CSS",
     link: "https://github.com/emeliesidesio/radio-player",
