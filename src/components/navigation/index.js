5b5885ea184cb13a3e089c76be73415e445b83c8
Emelie Sidesiö <esidesio@gmail.com>
2018-10-06 18:39:41 +0200

    code cleanup in navigation component, switching from px to rem

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,11 +1,10 @@
 import React from "react"
-import { Link } from "react-router-dom"
 import "./index.css"
 
 class Navigation extends React.Component {
   render() {
     return (
-      <div>
+      <div className="navigation">
         <input id="hamburger" type="checkbox" className="hamburger-checkbox" />
 
         <label className="hamburger" htmlFor="hamburger">
@@ -15,15 +14,9 @@ class Navigation extends React.Component {
         </label>
 
         <div className="menu">
-          <div className="nav-menu">
-            <ul className="nav-list">
-              <li><Link className="nav-link" to="/">Home</Link></li>
-              <li><Link className="nav-link" to="/resume">Resume</Link></li>
-              <li><a href="/#contact" className="nav-link">About</a></li>
-              <li><a href="/#contact" className="nav-link">Contact</a></li>
-            </ul>
-          </div>
+          <div><a href="/#contact" className="nav-link">About / Contact</a></div>
         </div>
+
       </div>
     )
   }
