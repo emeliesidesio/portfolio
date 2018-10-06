8242879ad0b44662c70d858eeea9f98a8c95587e
Emelie Sidesiö <esidesio@gmail.com>
2018-10-06 20:19:46 +0200

    adding a spacer component

---
+++ src/components/spacer/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./index.css"
+
+class Spacer extends React.Component {
+  render() {
+    return (
+      <div className="spacer" />
+    )
+  }
+}
+
+export default Spacer
