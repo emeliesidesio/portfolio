d02896b040c5620fad876334eaa6a79beed16769
Emelie Sidesiö <esidesio@gmail.com>
2018-10-07 16:57:33 +0200

    optimizing the project page

--- src/pages/project/index.js
+++ src/pages/project/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import projects from "data/projects"
 import Navigation from "components/navigation"
+import Spacer from "components/spacer"
 import Footer from "components/footer"
 import "./index.css"
 
@@ -26,15 +27,21 @@ export default class ProjectPage extends React.Component {
 
   render() {
     return (
-      <div className="ProjectPage">
+      <div>
         <Navigation />
         <div className="project-container">
           <div className="project-copy">
-            <h2>{this.state.project.name}</h2>
-            <p className="tagline">{this.state.project.language}</p><br />
-            <p>{this.state.project.description}</p><br />
-            <a href={this.state.project.demo} target="_blank" rel="noreferrer noopener"><p className="project-link">Live demo</p></a>
-            <a href={this.state.project.link} target="_blank" rel="noreferrer noopener"><p className="project-link">Project on Github</p></a>
+            <div className="title-secondary">{this.state.project.name}</div>
+            <div className="tagline">{this.state.project.language}</div>
+            <Spacer />
+            <div className="paragraph">{this.state.project.description}</div>
+            <Spacer />
+            <div>
+              <a className="project-link" href={this.state.project.demo} target="_blank" rel="noreferrer noopener">Live demo</a>
+            </div>
+            <div>
+              <a className="project-link" href={this.state.project.link} target="_blank" rel="noreferrer noopener">Project on Github</a>
+            </div>
           </div>
           <div className="project-img">
             <img src={this.state.project.image} className="project-detail-img" alt="" />
